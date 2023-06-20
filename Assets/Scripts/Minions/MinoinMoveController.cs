799831b1e7a550c6d0e19b9b7a87a999cb88c48d
Maksim <maximp95@mail.ru>
2023-06-20 23:12:05 +0300

    20.06

--- Assets/Scripts/Minions/MinoinMoveController.cs
+++ Assets/Scripts/Minions/MinoinMoveController.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MinoinMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float speed;
+    [SerializeField] private List<GameObject> points = new List<GameObject>();
+    private GameObject target;
+    private int indx;
+
+    private void Start()
     {
-        
+        indx = 0;
+        target = points[indx];
+        speed = 3f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (indx == points.Count)
+        {
+            return;
+        }
+        target = points[indx];
+        if (Vector3.Distance(transform.position, target.transform.position) >= 0.5f)
+        {
+            transform.LookAt(target.transform);
+            transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        }
+        else
+        {
+            indx++;
+            
+        }
     }
 }
