4b2dad809d1e23ed90170d0d93a70a9b9d0edcf8
poponin07 <maximp95@mail.ru>
2023-12-07 18:56:34 +0300

    07_12

---
+++ Assets/Scripts/BuildComponent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class BuildComponent : MonoBehaviour
+    {
+        private InputSystem_i _input;
+        private Ray ray;
+        [SerializeField] private CardManager m_cardManager;
+
+        private void Awake()
+        {
+            _input = new InputSystem_i();
+        }
+
+        private void Start()
+        {
+            _input.Camera.ClickForRayCast.canceled += context =>  OnRayCastPlayer();
+        }
+
+        private void OnRayCastPlayer()
+        {
+            Camera _camera = Camera.main;
+            ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                Card card = hit.transform.GetComponent<Card>();
+                if (card == null || card.players == RoundManager.instance.PlayerMove)
+                {
+                    СhoicePlayerAvatar сhoicePlayerAvatar = hit.transform.GetComponent<СhoicePlayerAvatar>();
+                    
+                    if (сhoicePlayerAvatar != null && сhoicePlayerAvatar.m_players ==  RoundManager.instance.PlayerMove && сhoicePlayerAvatar.isSetAvatar == false)
+                    {
+                        сhoicePlayerAvatar.SetAvatar();
+                        RoundManager.instance.MoveChange();
+                        return;
+                    }
+                   
+                }
+                Card cardComp = hit.transform != null ? card : null;
+                if (cardComp != null && cardComp.m_cardState == CardState.InHand && !RoundManager.instance.ferstMoveAnd)
+                {
+                    if (cardComp.CardInDeck(card))
+                    {
+                        m_cardManager.AddCardToDeck(cardComp);
+                        m_cardManager.GetCardFromDeck(1, true); 
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Killer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Killer : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(1);
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
