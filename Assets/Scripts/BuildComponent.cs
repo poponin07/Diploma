54594792472bf12927c479271e7812267af8d40b
poponin07 <maximp95@mail.ru>
2023-12-08 18:58:34 +0300

    08_12

--- Assets/Scripts/BuildComponent.cs
+++ Assets/Scripts/BuildComponent.cs
@@ -1,53 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
 
 namespace TowerDefense
 {
     public class BuildComponent : MonoBehaviour
     {
-        private InputSystem_i _input;
+        private InputController _input;
         private Ray ray;
-        [SerializeField] private CardManager m_cardManager;
 
         private void Awake()
         {
-            _input = new InputSystem_i();
+            _input = new InputController();
+            Debug.Log("Click");
         }
 
         private void Start()
         {
-            _input.Camera.ClickForRayCast.canceled += context =>  OnRayCastPlayer();
+            _input.Player.ClickForReycast.canceled += context =>  OnRayCastPlayer();
         }
 
         private void OnRayCastPlayer()
         {
+            
             Camera _camera = Camera.main;
             ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
             if (Physics.Raycast(ray, out var hit))
             {
-                Card card = hit.transform.GetComponent<Card>();
-                if (card == null || card.players == RoundManager.instance.PlayerMove)
+               if (hit.collider.GetComponent<MinoinMoveController>())
                 {
-                    СhoicePlayerAvatar сhoicePlayerAvatar = hit.transform.GetComponent<СhoicePlayerAvatar>();
-                    
-                    if (сhoicePlayerAvatar != null && сhoicePlayerAvatar.m_players ==  RoundManager.instance.PlayerMove && сhoicePlayerAvatar.isSetAvatar == false)
-                    {
-                        сhoicePlayerAvatar.SetAvatar();
-                        RoundManager.instance.MoveChange();
-                        return;
-                    }
-                   
-                }
-                Card cardComp = hit.transform != null ? card : null;
-                if (cardComp != null && cardComp.m_cardState == CardState.InHand && !RoundManager.instance.ferstMoveAnd)
-                {
-                    if (cardComp.CardInDeck(card))
-                    {
-                        m_cardManager.AddCardToDeck(cardComp);
-                        m_cardManager.GetCardFromDeck(1, true); 
-                    }
+                    Debug.Log("Zomby");
                 }
             }
         }
